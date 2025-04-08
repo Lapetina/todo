30fae9db0a1ab761847d0218a7aed3099348ec69
victorlapetina <lapetina.tech@gmail.com>
2025-04-07 21:02:40 -0300

    feat: create TaskService

---
+++ src/main/java/com/lapetina/todo/service/TaskService.java
@@ -0,0 +1,19 @@
+package com.lapetina.todo.service;
+
+import com.lapetina.todo.model.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskService {
+
+    Task createTask(Task task);
+
+    List<Task> getAllTasks();
+
+    Optional<Task> getTaskById(String id);
+
+    List<Task> getCompletedTasks();
+
+    void deleteTask(String id);
+}
---
+++ src/main/java/com/lapetina/todo/service/TaskServiceImpl.java
@@ -0,0 +1,45 @@
+package com.lapetina.todo.service;
+
+import com.lapetina.todo.model.Task;
+import com.lapetina.todo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Optional<Task> getTaskById(String id) {
+        return taskRepository.findById(id);
+    }
+
+    @Override
+    public List<Task> getCompletedTasks() {
+        return taskRepository.findByDoneTrue(); // usa o método custom do repository
+    }
+
+    @Override
+    public void deleteTask(String id) {
+        taskRepository.deleteById(id);
+    }
+}
