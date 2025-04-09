963fa46e8178c2ef5f10a15d0c593107b6891d08
victorlapetina <lapetina.tech@gmail.com>
2025-04-08 23:39:37 -0300

    feat: create TaskController.java

---
+++ src/main/java/com/lapetina/todo/controller/TaskController.java
@@ -0,0 +1,51 @@
+package com.lapetina.todo.controller;
+
+import com.lapetina.todo.model.Task;
+import com.lapetina.todo.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        return ResponseEntity.ok(taskService.createTask(task));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks() {
+        return ResponseEntity.ok(taskService.getAllTasks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable String id) {
+        return taskService.getTaskById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/done")
+    public ResponseEntity<List<Task>> getDoneTasks() {
+        return ResponseEntity.ok(taskService.getDoneTasks());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable String id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
